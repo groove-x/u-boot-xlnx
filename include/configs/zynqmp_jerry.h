672ecbb907e8ea6f8962a98494f2cc43cccc8670
puhitaku <puhitaku@gmail.com>
2019-04-18 14:28:27 +0900

    Define envs
    (cherry picked from commit a9a8e6542c65851e8585760feb6e7c697bbab4f8)

--- include/configs/zynqmp_jerry.h
+++ include/configs/zynqmp_jerry.h
@@ -28,6 +28,61 @@
 #define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
 #define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET	0x20
 
+#ifndef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"kernel_addr=0x80000\0" \
+	"initrd_addr=0xa00000\0" \
+	"initrd_size=0x2000000\0" \
+	"fdt_addr=4000000\0" \
+	"fdt_high=0x10000000\0" \
+	"loadbootenv_addr=0x100000\0" \
+	"sdbootdev=0\0"\
+	"kernel_offset=0x280000\0" \
+	"fdt_offset=0x200000\0" \
+	"kernel_size=0x1e00000\0" \
+	"fdt_size=0x80000\0" \
+	"uenvtxt_existence_test=test -e mmc $sdbootdev:$partid /uEnv.txt\0" \
+	"bootenv=uEnv.txt\0" \
+	"loadbootenv=load mmc $sdbootdev:$partid ${loadbootenv_addr} ${bootenv}\0" \
+	"timestamp_existence_test=test -e mmc $sdbootdev:$timestamp_partid /${timestamp}\0" \
+    "timestamp_partid=9\0" \
+	"timestamp=last_timestamp\0" \
+	"loadtimestamp=load mmc $sdbootdev:$timestamp_partid ${loadbootenv_addr} ${timestamp}\0" \
+    "append_timestamp=setenv bootargs $bootargs rtc-pseudo.timestamp=$last_timestamp\0" \
+	"importbootenv=echo Importing environment from SD ...; " \
+		"env import -t ${loadbootenv_addr} $filesize\0" \
+	"uenvboot=" \
+		"if run uenvtxt_existence_test; then " \
+			"run loadbootenv; " \
+			"echo Loaded environment from ${bootenv}; " \
+			"run importbootenv; " \
+		"fi; " \
+		"if run timestamp_existence_test; then " \
+			"run loadtimestamp; " \
+			"echo Loaded timestamp from ${timestamp}; " \
+			"run importbootenv; " \
+		"else; " \
+			"echo Warning: ${timestamp} is not found in mmc$sdbootdev:$timestamp_partid. Falling back to 1552102779.; " \
+			"setenv last_timestamp 1552102779; " \
+		"fi; " \
+		"if test -n $uenvcmd; then " \
+			"echo Running uenvcmd ...; " \
+			"run uenvcmd; " \
+		"fi\0" \
+	"sdboot=mmc dev $sdbootdev && mmcinfo && run uenvboot || run sdroot$sdbootdev; " \
+		"run append_timestamp; " \
+		"load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
+		"load mmc $sdbootdev:$partid $kernel_addr Image && " \
+		"booti $kernel_addr - $fdt_addr\0" \
+	"emmcboot=run sdboot\0" \
+	"jtagboot=tftpboot 80000 Image && tftpboot $fdt_addr system.dtb && " \
+		 "tftpboot 6000000 rootfs.cpio.ub && booti 80000 6000000 $fdt_addr\0" \
+	"sdroot0=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
+	"sdroot1=setenv bootargs $bootargs root=/dev/mmcblk1p2 rw rootwait\0" \
+	PARTS_DEFAULT \
+	DFU_ALT_INFO
+#endif
+
 #include <configs/xilinx_zynqmp.h>
 
 #endif /* __CONFIG_ZYNQMP_EIGER2_H */
