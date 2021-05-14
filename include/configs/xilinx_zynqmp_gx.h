d999480b7353357865a5ffb4607611a3e97c9068
Yoshifumi Hayashi <yoshifumi.hayashi@groove-x.com>
2021-05-14 17:21:24 +0900

    update uenv to read uEnv.Txt

--- include/configs/xilinx_zynqmp_gx.h
+++ include/configs/xilinx_zynqmp_gx.h
@@ -22,37 +22,39 @@
     "fdt_high=0x10000000\0" \
     "loadbootenv_addr=0x100000\0" \
     "sdbootdev=0\0"\
+    "uenvdev=0\0"\
     "kernel_offset=0x280000\0" \
     "fdt_offset=0x200000\0" \
     "kernel_size=0x1e00000\0" \
     "fdt_size=0x80000\0" \
     "bootenv=uEnv.txt\0" \
-    "loadbootenv=load mmc $sdbootdev:$partid ${loadbootenv_addr} ${bootenv}\0" \
-    "importbootenv=echo Importing environment from SD ...; " \
+    "loadbootenv=load mmc $uenvdevv:$partid ${loadbootenv_addr} ${bootenv}\0" \
+    "importbootenv=echo Importing environment ...; " \
         "env import -t ${loadbootenv_addr} $filesize\0" \
-    "sd_uEnvtxt_existence_test=test -e mmc $sdbootdev:$partid /uEnv.txt\0" \
+    "uenvexist=" \
+        "if test -e mmc 1:$partid /uEnv.txt; then " \
+            "env set uenvdev 1;" \
+        "elif test -e mmc 0:$partid /uEnv.txt; then " \
+            "env set uenvdev 0;" \
+        "else;" \
+            "env set uenvdev 2;" \
+        "fi\0" \
     "uenvboot=" \
-        "if run sd_uEnvtxt_existence_test; then " \
+        "run uenvexist;" \
+        "if test $uenvboot -ne 2; then " \
             "run loadbootenv; " \
-            "echo Loaded environment from ${bootenv}; " \
+            "echo Loaded environment from mmc${uenvboot}/${bootenv}; " \
             "run importbootenv; " \
         "fi; " \
         "if test -n $uenvcmd; then " \
             "echo Running uenvcmd ...; " \
             "run uenvcmd; " \
         "fi\0" \
-    "sdboot_nouenv=mmc dev $sdbootdev && run sdroot$sdbootdev; " \
-        "load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
-        "load mmc $sdbootdev:$partid $kernel_addr Image && " \
-        "booti $kernel_addr - $fdt_addr\0" \
-    "sdboot=mmc dev $sdbootdev && mmcinfo && run uenvboot || run sdroot$sdbootdev; " \
+    "sdboot=run uenvboot && mmc dev $sdbootdev && mmcinfo || run sdroot$sdbootdev; " \
         "load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
         "load mmc $sdbootdev:$partid $kernel_addr Image && " \
         "booti $kernel_addr - $fdt_addr\0" \
     "emmcboot=run sdboot\0" \
-    "nandboot=nand info && nand read $fdt_addr $fdt_offset $fdt_size && " \
-          "nand read $kernel_addr $kernel_offset $kernel_size && " \
-          "booti $kernel_addr - $fdt_addr\0" \
     "jtagboot=tftpboot 80000 Image && tftpboot $fdt_addr system.dtb && " \
          "tftpboot 6000000 rootfs.cpio.ub && booti 80000 6000000 $fdt_addr\0" \
     "nosmp=setenv bootargs $bootargs maxcpus=1\0" \
