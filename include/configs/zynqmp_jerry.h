c8a67634e270a45266309ad2886d5026b6c33068
Yoshifumi Hayashi <yoshifumi.hayashi@groove-x.com>
2021-08-27 18:30:52 +0900

    apply 210621 reglus patch (#5)
    * remove zynqmp-gx.dts
    
    * apply 210621 reglus patch
    
    * merge gx and jerry defconfig
    
    * Changed include file and Deleted unnecessary CONFIG options
    
    * Deleted gpio0 aliase
    
    Co-authored-by: Kenichi Nakamura <kenichi.nakamura@groove-x.com>

--- include/configs/zynqmp_jerry.h
+++ include/configs/zynqmp_jerry.h
@@ -10,10 +10,6 @@
 #ifndef __CONFIG_ZYNQMP_EIGER2_H
 #define __CONFIG_ZYNQMP_EIGER2_H
 
-#define CONFIG_ZYNQ_SDHCI0
-#define CONFIG_ZYNQ_SDHCI1
-#define CONFIG_ZYNQ_I2C0
-#define CONFIG_ZYNQ_I2C1
 #define CONFIG_SYS_I2C_MAX_HOPS		1
 #define CONFIG_SYS_NUM_I2C_BUSES	2
 #define CONFIG_SYS_I2C_BUSES	{ \
@@ -21,11 +17,7 @@
 				{1, {I2C_NULL_HOP} }, \
 				}
 
-#define CONFIG_SYS_I2C_ZYNQ
-
 #define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
-#define CONFIG_ZYNQ_EEPROM_BUS		0
-#define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
 #define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET	0x20
 
 #ifndef CONFIG_EXTRA_ENV_SETTINGS
@@ -91,7 +83,7 @@
 		"load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
 		"load mmc $sdbootdev:$partid $kernel_addr Image && " \
 		"booti $kernel_addr - $fdt_addr\0" \
-	"emmcboot=run sdboot\0" \
+	"modeboot=sdboot\0" \
 	"jtagboot=tftpboot 80000 Image && tftpboot $fdt_addr system.dtb && " \
 		 "tftpboot 6000000 rootfs.cpio.ub && booti 80000 6000000 $fdt_addr\0" \
 	"sdroot0=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
